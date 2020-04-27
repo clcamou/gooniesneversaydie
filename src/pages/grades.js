f641391e4a6e56aa54e2d4fe812ca63c4270819a
Andrew Bergheim <andrewbergheim@gmail.com>
2020-04-27 13:47:51 -0700

    disconnected api calls for grades and quizzes temporarily so the pages don't error out.

--- src/pages/grades.js
+++ src/pages/grades.js
@@ -3,12 +3,15 @@ import Assignment_Hub from "../components/Assignment_Hub"
 
 export default async function assignmenthub({ user }) {  
 
+    /* disconnected until API is set back up
     let data = await fetch("/api/getallstudentassignments", {
         method: "GET",
         body: {
             user_id: user.user_id
         }
     })
+    */
+   let data = []
     return (
      <Assignment_Hub data = {data}/>
     )
--- src/pages/quizzes.js
+++ src/pages/quizzes.js
@@ -2,11 +2,13 @@ import { useEffect, useRef } from 'react'
 import Quizzes_All from "../components/Quizzes_All"
 
 export default async function quizzes({user}) { 
-
+/* disconnected until API is set up again
     let data = await fetch("/api/getavailablequizzes", {
         method: "GET",
         body: {user}
     })
+*/
+let data = []
     return (
      <Quizzes_All data = {data}/>
     )
